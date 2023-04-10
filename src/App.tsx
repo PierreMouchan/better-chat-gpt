6fe912536eabf46c5ce4272e8ba845455bb52efb
Pierre Mouchan <pierre.mouchan@gmail.com>
2023-04-10 19:18:25 +0200

    refactor: refactor things like I wanted + adding pwa

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,21 @@ function App() {
     });
   }, []);
 
+  useEffect(()=>{
+    // Registering SW
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+          .then((registration) => {
+            console.log('Service worker registered:', registration);
+          })
+          .catch((error) => {
+            console.error('Error registering service worker:', error);
+          });
+      });
+    }
+  },[])
+
   useEffect(() => {
     // legacy local storage
     const oldChats = localStorage.getItem('chats');
--- src/components/Menu/MenuOptions/MenuOptions.tsx
+++ src/components/Menu/MenuOptions/MenuOptions.tsx
@@ -3,8 +3,6 @@ import useStore from '@store/store';
 
 import ClearConversation from './ClearConversation';
 import Api from './Api';
-import Me from './Me';
-import AboutMenu from '@components/AboutMenu';
 import ImportExportChat from '@components/ImportExportChat';
 import SettingsMenu from '@components/SettingsMenu';
 import CollapseOptions from './CollapseOptions';
@@ -19,12 +17,10 @@ const MenuOptions = () => {
           hideMenuOptions ? 'max-h-0' : 'max-h-full'
         } overflow-hidden transition-all`}
       >
-        <AboutMenu />
         <ClearConversation />
         <ImportExportChat />
         <Api />
         <SettingsMenu />
-        <Me />
       </div>
     </>
   );
